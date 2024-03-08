d6a353f979916b24459f51795ca2e9b07ef210c1
Hasan Nur Wakhid <hasannurwakhid@gmail.com>
2024-03-08 21:56:46 +0700

    Menambahkan sorting tahun

--- script/index.js
+++ script/index.js
@@ -77,57 +77,6 @@ buttonCariMobil.addEventListener("submit", (event) => {
   let searchedCars = cars.searchCars(tanggal.value, jumlah_penumpang.value);
   let searchedCars2 = [...searchedCars];
 
-  // let cardSearchAvailable = "";
-  // searchedCars.map((car) => {
-  //   cardSearchAvailable += `<div class="col-md-4 d-flex">
-  //               <div class="card mb-4 flex-fill">
-  //                 <div class="card-body p-4 d-flex flex-column">
-  //                   <img
-  //                     src="${car.image}"
-  //                     class="img-fluid mb-4 rounded"
-  //                     alt="..."
-  //                   />
-  //                   <p class="card-text">${car.model}</p>
-  //                   <h5 class="card-title mb-3">Rp ${car.rentPerDay
-  //                     .toLocaleString()
-  //                     .replace(",", ".")} / hari</h5>
-  //                   <p class="card-text">
-  //                     ${car.description}
-  //                   </p>
-  //                   <div class="mb-2">
-  //                     <img class="me-2" src="assets/fi_users.png" alt="" />${
-  //                       car.capacity
-  //                     }
-  //                     Orang
-  //                   </div>
-  //                   <div class="mb-2">
-  //                     <img
-  //                       class="me-2"
-  //                       src="assets/fi_settings.png"
-  //                       alt=""
-  //                     />${car.transmission}
-  //                   </div>
-  //                   <div class="mb-2">
-  //                     <img
-  //                       class="me-2"
-  //                       src="assets/fi_calendar.png"
-  //                       alt=""
-  //                     />Tahun ${car.year}
-  //                   </div>
-  //                   <div class="mt-auto">
-  //                   <a
-  //                     href="#"
-  //                     class="btn col-12 text-white"
-  //                     style="background-color: #5cb85f"
-  //                     >Pilih Mobil</a
-  //                   >
-  //                   </div>
-  //                 </div>
-  //               </div>
-  //             </div>`;
-  // });
-  // cardCars.innerHTML = cardSearchAvailable;
-
   fungsiMenampilkan(searchedCars);
 
   urutkanTahun.addEventListener("change", (event) => {
